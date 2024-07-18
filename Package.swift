02282db08613c9dd6f96859e7fcc2a4a0e8d7080
rayhaanalykhan <rayhaanalykhan@gmail.com>
2024-07-18 05:09:42 +0500

    Add strict concurrency check

--- Package.swift
+++ Package.swift
@@ -28,7 +28,10 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CipherEncryption",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [
+                .unsafeFlags(["-strict-concurrency=complete"])
+            ]
         ),
         
         
